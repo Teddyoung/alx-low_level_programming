ebf1b3e500b31835093eb11099efa8958bc1ff46
Dare Oluwatosin <daretosin97@gmail.com>
2022-09-09 21:23:30 -0700

    hello

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	char C = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
