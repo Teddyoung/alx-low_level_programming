dcf3164262f4ff4fdc7660c5e1b5ea53da846e2c
Dare Oluwatosin <daretosin97@gmail.com>
2022-09-13 17:18:34 -0700

    hello

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,7 +4,7 @@
 
 /**
  * main - main block
- * Description: Print the sum of all multiples of 3 or 5 below
+ * Description: Print the sum of all multiples of 3 or 5 below 1024.
  * Return: 0
  */
 
