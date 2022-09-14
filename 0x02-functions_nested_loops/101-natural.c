ca460bb7bb8ade892b903cf173dfc6f5d127ac6e
Dare Oluwatosin <daretosin97@gmail.com>
2022-09-13 17:26:18 -0700

    hello

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -15,13 +15,13 @@ int main(void)
 
 	while (i < 1024)
 	{
-	if (i % 3 == 0)
+		if (i % 3 == 0)
 
-		total +=i;
-	else if (i % 5 == 0)
-		total += i;
+			total +=i;
+		else if (i % 5 == 0)
+			total += i;
 
-	i++;
+		i++;
 	}
 	printf("%d\n". total);
 
