5834856cf54c6eb73321bc21c2dd6be22ca121f5
Dare Oluwatosin <daretosin97@gmail.com>
2022-10-06 13:58:18 -0700

    hello

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,7 +5,7 @@
  * @s1: pointer to first string
  * @s2: pointer to second string
  * @n: number of bytes to use from s2
- *
+ * 
  * Return: pointer to newly created string, else NULL if failure occurs
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
