57488dd8766ca2037610d4f4253068a00b132707
Dare Oluwatosin <daretosin97@gmail.com>
2022-10-11 01:40:25 -0700

    hello

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,17 @@
+nclude "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ *  main - check the code for Holberton School students.
+ *
+ *  return: Always 0.
+ */
+int main(void)
+{
+	int s;
+
+	s = 98 + SIZE;
+	printf("%d\n", s);
+	return (0);
+}
